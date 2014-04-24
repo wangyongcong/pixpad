135ca044b28f11fd4bca0ddb99c36efed4f281e4
wangyongcong <wangyongcong@gmail.com>
2014-04-25 00:33:14 +0800

    Model rotation

--- pixpad/windows/wmain.cpp
+++ pixpad/windows/wmain.cpp
@@ -2,6 +2,7 @@
 #include "resource.h"
 #include <cstdio>
 #include <tuple>
+#include <algorithm>
 #include "util/log.h"
 #include "glrender.h"
 #include "glpipeline.h"
@@ -33,7 +34,6 @@ struct AppContext
 {
 	HINSTANCE instance = NULL;
 	HWND main_wnd = NULL;
-	bool is_size_changed = false;
 	// renderer
 	wyc::xpipeline *pipelines[RENDERER_COUNT];
 	RENDER_MODE render_mode = OPENGL_RENDERER;
@@ -42,7 +42,11 @@ struct AppContext
 	std::pair<wyc::xvertex_buffer*, wyc::xindex_buffer*> models[MODEL_COUNT];
 	MODEL_ID model_id = REGULAR_TRIANGLE;
 	std::string material;
-
+	// app state
+	bool is_size_changed = false;
+	bool is_drag_mode = false;
+	std::pair<int, int> drag_start_pos;
+	wyc::xvec4f_t rotate;
 	AppContext()
 	{
 		for (int i = 0; i < RENDERER_COUNT; ++i)
@@ -54,6 +58,7 @@ struct AppContext
 			models[i].second = nullptr;
 		}
 		transform.identity();
+		rotate.zero();
 	}
 
 	~AppContext()
@@ -91,6 +96,8 @@ void				OnResizeWindow (int, int);
 void CALLBACK		TimerFlushLog(HWND, UINT, UINT_PTR, DWORD);
 void				OnRender();
 void				SetRenderMode(RENDER_MODE mode);
+void				OnRotateModel(int dx, int dy);
+void				OnRotateReset();
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -246,7 +253,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	}
 
 	// Set timer for log flushing 
-	SetTimer(hWnd, ID_TIMER_LOG, 1000, &TimerFlushLog);
+	SetTimer(hWnd, ID_TIMER_LOG, 500, &TimerFlushLog);
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
@@ -257,12 +264,16 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 // Main window
 //
+static const std::unordered_map<int, RENDER_MODE> s_menuid_to_renderer = {
+	{ IDM_SOFTWARE, SOFTWARE_RENDERER },
+	{ IDM_OPENGL, OPENGL_RENDERER },
+};
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
 	HDC hdc;
-
+	wyc::xpipeline *pipeline;
 	switch (message)
 	{
 	case WM_COMMAND: // Window menu
@@ -272,12 +283,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		switch (wmId)
 		{
 		case IDM_OPENGL:
-			CheckMenuRadioItem(GetMenu(g_app.main_wnd), IDM_SOFTWARE, IDM_OPENGL, IDM_OPENGL, MF_BYCOMMAND);
-			SetRenderMode(OPENGL_RENDERER);
-			break;
 		case IDM_SOFTWARE:
-			CheckMenuRadioItem(GetMenu(g_app.main_wnd), IDM_SOFTWARE, IDM_OPENGL, IDM_SOFTWARE, MF_BYCOMMAND);
-			SetRenderMode(SOFTWARE_RENDERER);
+			CheckMenuRadioItem(GetMenu(g_app.main_wnd), IDM_SOFTWARE, IDM_OPENGL, wmId, MF_BYCOMMAND);
+			SetRenderMode(s_menuid_to_renderer.at(wmId));
 			break;
 		case IDM_ABOUT:
 			DialogBox(g_app.instance, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
@@ -304,6 +312,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		if (g_app.is_size_changed) {
 			g_app.is_size_changed = false;
 		}
+		break;
+	case WM_LBUTTONDOWN:
+		SetCapture(g_app.main_wnd);
+		g_app.is_drag_mode = true;
+		g_app.drag_start_pos = { LOWORD(lParam), HIWORD(lParam) };
+		break;
+	case WM_LBUTTONUP:
+		g_app.is_drag_mode = false;
+		g_app.rotate.x = g_app.rotate.z;
+		g_app.rotate.y = g_app.rotate.w;
+		ReleaseCapture();
+		break;
+	case WM_MOUSEMOVE:
+		if (g_app.is_drag_mode) 
+			OnRotateModel(LOWORD(lParam) - g_app.drag_start_pos.first, \
+				HIWORD(lParam) - g_app.drag_start_pos.second);
+		break;
+	case WM_MOUSEWHEEL:
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_SPACE)
+			OnRotateReset();
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -362,8 +392,10 @@ void OnRender()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	wyc::xpipeline *pipeline = g_app.pipelines[g_app.render_mode];
-	if (pipeline)
+	if (pipeline) {
+		pipeline->set_transform(g_app.transform);
 		pipeline->render();
+	}
 	wyc::gl_get_context()->swap_buffers();
 }
 
@@ -387,3 +419,34 @@ void SetRenderMode(RENDER_MODE mode)
 	pipeline->set_transform(g_app.transform);
 	pipeline->set_material(g_app.material);
 }
+
+void OnRotateModel(int dx, int dy)
+{
+	float speed = 0.5f;
+	float xrot = dy*speed + g_app.rotate.x, \
+		yrot = dx*speed + g_app.rotate.y;
+	xrot = std::max<float>(-89, std::min<float>(xrot, 89));
+	if (yrot >= 360)
+		yrot -= 360;
+	g_app.rotate.z = xrot;
+	g_app.rotate.w = yrot;
+	wyc::xmat3f_t m1, m2;
+	wyc::matrix_yrotate3d(m1, DEG_TO_RAD(yrot));
+	wyc::matrix_xrotate3d(m2, DEG_TO_RAD(xrot));
+	m1.mul(m2);
+	wyc::xvec3f_t v;
+	for (int i = 0; i < 3; ++i)
+	{
+		m1.get_row(i, v);
+		g_app.transform.set_col(i, v);
+	}
+}
+
+void OnRotateReset()
+{
+	g_app.transform.set_row(0, wyc::xvec3f_t(1, 0, 0));
+	g_app.transform.set_row(1, wyc::xvec3f_t(0, 1, 0));
+	g_app.transform.set_row(2, wyc::xvec3f_t(0, 0, 1));
+	g_app.rotate.zero();
+}
+
