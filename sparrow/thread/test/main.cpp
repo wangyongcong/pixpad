f925b8519f248aee679fb5f401a2c769342b2da0
ycwang <wangyongcong@gmail.com>
2017-03-20 00:57:54 +0800

    Test bytemaster's disruptor
    share_write_cursor is wrong, need to be fixed

--- sparrow/thread/test/main.cpp
+++ sparrow/thread/test/main.cpp
@@ -5,14 +5,16 @@
 #include "spsc_queue.h"
 #include "ring_queue.h"
 #include "folly_queue.h"
+#include "disruptor.h"
 
 int main()
 {
 	//RUN_TEST(lockfree_rb_q);
 	//RUN_TEST(mpmc_queue);
-	RUN_TEST(spsc_queue);
-	RUN_TEST(folly_queue);
-	RUN_TEST(ring_queue);
+	//RUN_TEST(spsc_queue);
+	//RUN_TEST(folly_queue);
+	//RUN_TEST(ring_queue);
+	RUN_TEST(disruptor_queue);
 
 	::system("pause");
 }
---
+++ sparrow/thread/test/test_disruptor.cpp
@@ -0,0 +1,133 @@
+#include "disruptor.h"
+#include <cassert>
+#include <iostream>
+#include <thread>
+#include <future>
+
+UNIT_TEST_BEG(disruptor_queue)
+
+#include "common.h"
+
+struct Event
+{
+	alignas(64) int id;
+	alignas(64) int c1;
+	alignas(64) int c2;
+};
+
+void test()
+{
+	using namespace disruptor;
+	
+	constexpr int SIZE = 256;
+	constexpr int COUNT = 4096;
+
+	ring_buffer<int, SIZE> buff;
+
+	auto sw = std::make_shared<shared_write_cursor>("SW", SIZE);
+	auto r1 = std::make_shared<read_cursor>("r1");
+	auto r2 = std::make_shared<read_cursor>("r2");
+	r1->follows(sw);
+	r2->follows(sw);
+	sw->follows(r1);
+	sw->follows(r2);
+
+	Event data[COUNT];
+	memset(data, sizeof(Event)*COUNT, 0);
+
+	std::promise<void> p_start, c_start;
+	std::shared_future<void> p_wait_start(p_start.get_future());
+	std::shared_future<void> c_wait_start(c_start.get_future());
+
+	auto p1 = std::thread([&, p_wait_start] {
+		p_wait_start.wait();
+		printf("p1 start\n");
+		for (int i = 0; i < COUNT / 2; ++i)
+		{
+			auto start = sw->claim(1);
+			data[i].id = i + 1;
+			buff.at(start) = i;
+			printf("p1 publish %d\n", start);
+			sw->publish_after(start, start - 1);
+		}
+	});
+
+	auto p2 = std::thread([&, p_wait_start] {
+		p_wait_start.wait();
+		printf("p2 start\n");
+		for (int i = COUNT / 2; i < COUNT; ++i)
+		{
+			auto start = sw->claim(1);
+			data[i].id = i + 1;
+			buff.at(start) = i;
+			printf("p2 publish %d\n", start);
+			sw->publish_after(start, start - 1);
+		}
+	});
+
+	auto c1 = std::thread([&, c_wait_start] {
+		c_wait_start.wait();
+		printf("c1 start\n");
+		auto pos = r1->begin();
+		auto end = r1->end();
+		int i = 0;
+		while (i < COUNT - 1)
+		{
+			if (pos == end)
+			{
+				r1->publish(end - 1);
+				printf("c1 wait for %d\n", end);
+				end = r1->wait_for(end);
+			}
+			i = buff.at(pos);
+			data[i].c1 = 1;
+			//printf("c1: %d\n", data[i].id);
+			++pos;
+		}
+	});
+
+	auto c2 = std::thread([&, c_wait_start] {
+		c_wait_start.wait();
+		printf("c2 start\n");
+		auto pos = r2->begin();
+		auto end = r2->end();
+		int i = 0;
+		while (i < COUNT - 1)
+		{
+			if (pos == end)
+			{
+				r2->publish(end - 1);
+				printf("c2 wait for %d\n", end);
+				end = r2->wait_for(end);
+			}
+			i = buff.at(pos);
+			data[i].c2 = 1;
+			//printf("c2: %d\n", data[i].id);
+			++pos;
+		}
+	});
+
+	printf("start producer...\n");
+	p_start.set_value();
+	printf("start consumer...\n");
+	c_start.set_value();
+
+	p1.join();
+	printf("p1 end\n");
+	p2.join();
+	printf("p2 end\n");
+	c1.join();
+	printf("c1 end\n");
+	c2.join();
+	printf("c2 end\n");
+
+	for (int i = 0; i < COUNT; ++i)
+	{
+		assert(data[i].c1);
+		assert(data[i].c2);
+	}
+
+	printf("test done!\n");
+}
+
+UNIT_TEST_END
