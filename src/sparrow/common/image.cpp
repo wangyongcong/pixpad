3e78b5dd729e4ed482adb74c1674ae89a935b915
ycwang <wangyongcong@gmail.com>
2017-06-14 00:15:35 +0800

    import stb image module

--- src/sparrow/common/image.cpp
+++ src/sparrow/common/image.cpp
@@ -1,4 +1,11 @@
 #include "image.h"
+#pragma warning(disable: 4996)
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include <png.h>
 #include "log.h"
 #include "util.h"
