447a9fe876e1c4768db772cf6a25dfda5c62faeb
ycwang <wangyongcong@gmail.com>
2017-11-10 13:58:46 +0800

    use tinyply for ply files

--- src/testbed/test_depth.cpp
+++ src/testbed/test_depth.cpp
@@ -1,10 +1,23 @@
 #pragma once
+#include <fstream>
 #include "test.h"
+#include "tinyply.h"
 
 class CTestDepth : public CTest
 {
 public:
-	virtual void run() {
+	virtual void run() 
+	{
+		std::ifstream fs("res/torus.ply");
+		tinyply::PlyFile file(fs);
+		for (auto e : file.get_elements())
+		{
+			for (auto p : e.properties)
+			{
+				auto &prop = tinyply::PropertyTable[p.propertyType];
+			}
+		}
+
 	}
 
 private:
